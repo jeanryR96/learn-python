bc03f8a679ccd025300e37a71aaef2f4f5583054
jeanryR96 <sutidy.python96@gmail.com>
2023-08-03 09:49:06 +0700

    control-flow

---
+++ control-flow-statement/if_statement.py
@@ -0,0 +1,8 @@
+# Control flow statement
+
+firstInput = input("Enter here your first input: ")  # Input data from user
+secondInput = input("Enter here your second input: ")
+
+result = firstInput * secondInput
+
+print("Result of ", firstInput, " x ", secondInput, " = ", result)
