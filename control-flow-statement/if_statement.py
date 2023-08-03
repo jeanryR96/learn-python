77abae19c7c2271a75ee035ae0da2113e34b7b47
jeanryR96 <sutidy.python96@gmail.com>
2023-08-03 09:55:27 +0700

    if-statement.py change

--- control-flow-statement/if_statement.py
+++ control-flow-statement/if_statement.py
@@ -3,6 +3,7 @@
 firstInput = input("Enter here your first input: ")  # Input data from user
 secondInput = input("Enter here your second input: ")
 
-result = firstInput * secondInput
-
-print("Result of ", firstInput, " x ", secondInput, " = ", result)
+result = (float(firstInput) * float(secondInput))
+if result > 10:
+    print("Result of ", firstInput, " x ",
+          secondInput, " = ", result, "\nGrade is A")
