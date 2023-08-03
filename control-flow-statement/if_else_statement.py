ab6e8f734b735796d4753a68faff29b045d45a05
jeanryR96 <sutidy.python96@gmail.com>
2023-08-03 10:19:19 +0700

    first commit

---
+++ control-flow-statement/if_else_statement.py
@@ -0,0 +1,11 @@
+# Control flow statement 'if'
+
+firstInput = input("Enter here your course 1: ")  # Input data from user
+secondInput = input("Enter here your course 2: ")
+
+result = (float(firstInput) + float(secondInput))
+if result > 95:
+    print("Result of ", firstInput, " x ",
+          secondInput, " = ", result, "\nYour 'Excelent'")
+else:
+    print("Is Bad")
